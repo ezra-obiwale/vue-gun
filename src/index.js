23f5198f6cc9010cf47b07ebadfda59f66dba652
Ezra Obiwale <contact@ezraobiwale.com>
2018-08-30 21:16:38 +0100

    Updates
    - Added gun dependency
    - Fixed VueWatcher

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-
+import Gun from 'gun/gun'
 export default {
     install: function (Vue, options) {
       Vue.prototype.$gun = Gun(options)
       var $gunData = {
-        arrToGunObj: function (a) {
-          var o = { _isArr: true }, l = a.length;
-          for (var i = 0; i < l; i++) if (a[i] !== undefined) o[i] = a[i];
-          return o;
-        },
         addGunWatcher: function (keyPath, val, vm) {
           vm.$gun.get(vm.$options.$gunRootKey).path(keyPath)
-            .not(function () { 
-                this.put(typeof val == 'object' && val.constructor === Array ? this.arrToGunObj(val) : val) 
+            .not(function () {
+                this.put(typeof val == 'object' && val.constructor === Array ? this.arrToGunObj(val) : val)
             })
             .on(function (gunVal, gunKey) {
               if (typeof gunVal == 'object') return; /* only do stuff on non objects */
@@ -27,43 +22,64 @@ export default {
                 }
             }, { change: true });
         },
-        timeout: {},
-        vueWatcher: {},
-        addVueWatcher: function (keyPath, vm) {
+        addVueWatcher: function (keyPath, isArray, vm) {
           this.vueWatcher[keyPath] = vm.$watch(keyPath, function (newVal) {
             clearTimeout(this.timeout[vm.$options.$gunRootKey + keyPath]);
-            this.timeout[vm.$options.$gunRootKey + keyPath] = setTimeout(function () { vm.$gun.get(vm.$options.$gunRootKey).path(keyPath).put(newVal) }, 500);
-          });
+            this.timeout[vm.$options.$gunRootKey + keyPath] = setTimeout(function () {
+              if (isArray) {
+                let newIndex = newVal.findIndex(val => !val.$key)
+                if (newIndex > -1) {
+                  this.watch(keyPath, newIndex, newVal[newIndex], vm)
+                }
+              } else {
+                vm.$gun.get(vm.$options.$gunRootKey).path(keyPath).put(newVal)
+              }
+            }, 500);
+          }.bind(this));
         },
         addWatchers: function (obj, keyPath, vm) {
           if (keyPath) keyPath += '.';
           obj = Object.assign({}, obj)
-          var objKeys = Object.keys(a), 
-            key = '', 
+          var objKeys = Object.keys(obj),
+            key = '',
             value = '';
-          for (var i = 0; i < objKeys.length, key = objKeys[i], value = a[key]; i++) {
-            if (typeof value == 'object') {
-              this.addWatchers(value, keyPath + key, vm);
-            } else {
-              if (!this.vueWatcher[keyPath + key]) {
-                this.addVueWatcher(keyPath + key, vm);
-                this.addGunWatcher(keyPath + key, value, vm);
-              }
+          for (var i = 0; i < objKeys.length, key = objKeys[i], value = obj[key]; i++) {
+            this.watch(keyPath, key, value, vm)
+            if (Array.isArray(value)) {
+              this.addWatchers(value,  keyPath + key, vm);
             }
+          }
+        },
+        arrToGunObj: function (a) {
+          var o = { _isArr: true }, l = a.length;
+          for (var i = 0; i < l; i++) if (a[i] !== undefined) o[i] = a[i];
+          return o;
+        },
+        timeout: {},
+        vueWatcher: {},
+        watch: function (keyPath, key, value, vm) {
+          if (!this.vueWatcher[keyPath + key]) {
+            this.addVueWatcher(keyPath + key, Array.isArray(value), vm);
+            this.addGunWatcher(keyPath + key, value, vm);
           }
         }
       }
       Vue.mixin({
         data: function () {
-          var dataObj = {}, gunData = this.$options.gunData;
+          if (!this.$options.gunData) {
+            return {}
+          }
+          var dataObj = {}, gunData = typeof this.$options.gunData == 'function' ? this.$options.gunData() : this.$options.gunData;
           this.$options.$gunRootKey = (gunData._rootKey || window.location.hostname).replace(/\./g, '-');
           delete gunData._rootKey;
-          for (var key = '', value = '', i = 0, objKeys = Object.keys(gunData); i < objKeys.length, key = objKeys[i], value = gunData[key]; i++) dataObj[key] = value;
+          for (var key = '', value = '', i = 0, objKeys = Object.keys(gunData); i < objKeys.length, key = objKeys[i], value = gunData[key]; i++) {
+            dataObj[key] = value;
+          }
           return dataObj;
         },
         created: function () {
           if (this.$options.gunData) {
-            $gunData.addWatchers(this.$options.gunData, '', this)
+            $gunData.addWatchers(typeof this.$options.gunData == 'function' ? this.$options.gunData() : this.$options.gunData, '', this)
           }
         }
       });
